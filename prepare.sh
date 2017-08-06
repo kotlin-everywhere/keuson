280c862d495fd201584cc87dfd8698b748572316
Park Sung Min <vamf12@gmail.com>
2017-08-07 01:04:53 +0900

    java, js -> keuson-java, keuson-js

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "rootProject.name='keuson'\ninclude 'java', 'js'" > settings.gradle
+echo "rootProject.name='keuson'\ninclude 'keuson-java', 'keuson-js'" > settings.gradle
 touch build.gradle
 java/gradlew wrapper
